85beba72cb54c894967ba5b77be86592a441ba9f
Kai <panchaldevraj325@gmail.com>
2026-05-30 10:37:13 +0530

    Update profile.ps1

--- Terminal/PowerShell/profile.ps1
+++ Terminal/PowerShell/profile.ps1
@@ -10,5 +10,5 @@ Clear-Host
 
 # Force Fastfetch to use YOUR config every time (bypass path confusion)
 if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
-    fastfetch -c "C:/Users/%USERPROFILE%/.config/fastfetch/config.jsonc"
+    fastfetch -c "C:/Users/Kai/.config/fastfetch/config.jsonc"
 }
